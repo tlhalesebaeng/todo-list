807b91ce56b2e748b668caa8d604078ca54b36ab
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-06-03 19:54:59 +0200

    configuring globals to be able to use expect properly

--- todo-list/vitest.setup.js
+++ todo-list/vitest.setup.js
@@ -1,5 +1,6 @@
 import { afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
 // this will automatically unmount react trees after each test
 afterEach(() => {
