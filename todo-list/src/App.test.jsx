356d24a2d1bd15af26af7f655dbbd41af0ae6f3a
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-06-05 17:05:00 +0200

    test if clicking the button adds a new task

--- todo-list/src/App.test.jsx
+++ todo-list/src/App.test.jsx
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 import App from './App.jsx';
+import userEvent from '@testing-library/user-event';
+import { TaskContextProvider } from './store/tasks-context.jsx';
 
 describe('App', () => {
     it('renders "To Do List" title', () => {
@@ -21,5 +23,26 @@ describe('App', () => {
         expect(addButtonElement).toBeInTheDocument();
     });
 
-    // it('adds the task on add button click', () => {});
+    it('adds the task on add button click', async () => {
+        // render the app with the provider
+        render(
+            <TaskContextProvider>
+                <App />
+            </TaskContextProvider>
+        );
+
+        // get the input field element and fill it with a title
+        const titleInputElement = screen.getByPlaceholderText(/task title/i);
+        await userEvent.type(titleInputElement, 'test name');
+
+        // get the button to be clicked and click it
+        const addButtonElement = screen.getByRole('button', { name: '+' });
+        await userEvent.click(addButtonElement);
+
+        // get the newly added task
+        const taskElement = screen.getByText(/test name/i);
+
+        // check if a task with the filled with test name is in the document
+        expect(taskElement).toBeInTheDocument();
+    });
 });
